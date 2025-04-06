edfaa6ea6751565c64e50829c897faa67abacf8e
Gguife <gguife747@gmail.com>
2025-04-06 01:58:47 -0300

    The solve me first hackerrank exercise resolved

---
+++ src/hackerrank/ProblemSolved/SolveMeFirst.java
@@ -0,0 +1,13 @@
+package hackerrank.ProblemSolved;
+
+public class SolveMeFirst {
+   public static void main(String[] args) {
+      SolveMeFirst solveMeFirst = new SolveMeFirst();
+      int resultSum = solveMeFirst.sum(7, 3);
+      System.out.println("The sum of two numbers is: " + resultSum);
+   }
+
+   public int sum(int a, int b) {
+      return a + b;
+   }
+}
