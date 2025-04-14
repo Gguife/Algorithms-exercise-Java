b5a4d9e9c0bcc5427dedca68816d7ba82aaff55e
Gguife <gguife747@gmail.com>
2025-04-14 17:38:03 -0300

    Left rotation hackerrank exercise is solved

---
+++ src/hackerrank/DS/LeftRotation.java
@@ -0,0 +1,30 @@
+package hackerrank.DS;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LeftRotation {
+
+   public static void main(String[] args) {
+      int[] arr = {1, 2, 3, 4, 5};
+      int d = 2;
+
+      int[] result = leftRotation(d, arr);
+
+      System.out.println(Arrays.toString(result));
+   }
+
+   public static int[] leftRotation(int d, int[] arr) {
+      int n = arr.length;
+      int[] rotatedArray = new int[n];
+
+      for(int i = 0; i < n; i++) {
+         //Calculate the new position for each element
+         int newIndex = (i + n - d) % n;
+         rotatedArray[newIndex] = arr[i];
+      }
+
+      return rotatedArray;
+   }
+}
