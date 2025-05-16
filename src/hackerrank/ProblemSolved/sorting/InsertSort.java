8732520157a046a374c4e4d5b69c733aa31fd4fb
Gguife <gguife747@gmail.com>
2025-05-16 11:58:57 -0300

    add a part 2 of sorting exercise hackerrank problem

--- src/hackerrank/ProblemSolved/sorting/InsertSort.java
+++ src/hackerrank/ProblemSolved/sorting/InsertSort.java
@@ -2,15 +2,16 @@ package hackerrank.ProblemSolved.sorting;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class InsertSort {
 
     public static void main(String[] args) {
-        List<Integer> arr = new ArrayList<>(Arrays.asList(1, 2, 4, 5, 3));
-        insertionSort1(5, arr);
+        List<Integer> arr = new ArrayList<>(Arrays.asList(1, 4, 3, 5, 2));
+//        insertionSort1(5, arr);
+//        insertionSort2(5, arr);
     }
+
 
     public static void insertionSort1(int n, List<Integer> arr) {
         int value = arr.get(n - 1);
@@ -34,4 +35,23 @@ public class InsertSort {
         }
         System.out.println();
     }
+
+
+    public static void insertionSort2(int n, List<Integer> arr) {
+        for(int j = 1; j < n; j++) {
+            int key = arr.get(j);
+            int i = j - 1;
+
+            while(i >=0 && arr.get(i) > key) {
+                arr.set(i + 1, arr.get(i));
+                i = i - 1;
+            }
+
+            arr.set(i + 1, key);
+
+            for(int num : arr) System.out.print(num + " ");
+            System.out.println();
+        }
+    }
+
 }
