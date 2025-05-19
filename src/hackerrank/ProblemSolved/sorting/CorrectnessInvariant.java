d18fe686925ebbcb3073265c47ec5bac850cf2af
Gguife <gguife747@gmail.com>
2025-05-19 18:13:55 -0300

    Resolved correct invariant hackerrank sort problem

---
+++ src/hackerrank/ProblemSolved/sorting/CorrectnessInvariant.java
@@ -0,0 +1,29 @@
+package hackerrank.ProblemSolved.sorting;
+
+public class CorrectnessInvariant {
+
+    public static void insertionSort(int[] A){
+        for(int i = 1; i < A.length; i++){
+            int value = A[i];
+            int j = i - 1;
+            while(j >= 0 && A[j] > value){
+                A[j + 1] = A[j];
+                j = j - 1;
+            }
+            A[j + 1] = value;
+        }
+
+        printArray(A);
+    }
+
+
+    static void printArray(int[] ar) {
+        for(int n: ar){
+            System.out.print(n+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] ar = {2, 3, 5, 4, 1, 6};
+        insertionSort(ar);
+    }
+}
